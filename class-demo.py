4ab57343f523cf28bdcfeffc53df5ba973d1663d
rayees2021 <rayeesacc@gmail.com>
2021-06-23 12:46:22 +0100

    -m "class-demo"

---
+++ class-demo.py
@@ -0,0 +1,10 @@
+class product:
+    def __init__(self):
+        self.name = 'iphone'
+        self.desc = "It is awsome"
+        self.price= "10000"
+
+p1 = product()
+print(p1.name)
+print(p1.desc)
+print(p1.price)
